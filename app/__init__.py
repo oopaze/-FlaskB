bb9df1aff113a9810632b8829060f5fca5962db0
oopaze <pedroosd28@gmail.com>
2020-10-07 00:27:28 -0300

    fuck

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR}/storage.sqlite3'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
